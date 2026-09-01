db572d85d8af05f3e8c73d6d22be4ef93d607c64
luke-j-davis <lukeloop101@gmail.com>
2026-09-01 13:16:01 +1000

    Had warning Also did some extra chnages as it still failed
    Checked what it was and its cause v4 isnt used anymore but github I believe.

--- Luke/database/app.py
+++ Luke/database/app.py
@@ -130,7 +130,7 @@ def create_bill():
 def health_check():
     return jsonify({
         "status": "ok",
-        "service": "bills-backend-API"
+        "service": "bills-database-API"
     })
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
